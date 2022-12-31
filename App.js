0a944619ec1ce18cc73370aaa27f17dd93b3865f
engineersonal <engineersonal@gmail.com>
2022-12-31 22:26:30 +0100

    Custom CRA using Parcel bundler

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const heading1 = React.createElement('h1', {id: "heading", key: "1"}, 'React bootstrap using CDN')
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement('h1', {id: "heading", key: "1"}, 'React bootstrap using custom CRA through Parcel')
 const heading2 = React.createElement('h2', {id: "subheading", key: "2"}, 'By Sonal Sharma')
 const container = React.createElement('div', {id: 'container'}, [heading1, heading2])
 const root = ReactDOM.createRoot(document.getElementById('root'))
